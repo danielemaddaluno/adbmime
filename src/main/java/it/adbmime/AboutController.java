f3e491d2c633fbf0b0ba278cce016184222aa422
Daniele Maddaluno <daniele.maddaluno@gmail.com>
2022-02-10 15:44:00 +0100

    Added simple about.fxml and centered graphical elements on adbmime-view.fxml

---
+++ src/main/java/it/adbmime/AboutController.java
@@ -0,0 +1,22 @@
+package it.adbmime;
+
+import java.io.IOException;
+import it.adbmime.web.WebViewUtils;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+
+public class AboutController {
+
+	@FXML
+	private Hyperlink hyperlink; 
+	
+    @FXML
+    private void openPrimary() throws IOException {
+        AdbMimeApplication.setRoot("adbmime-view");
+    }
+    
+    @FXML
+	private void openLink() {
+    	WebViewUtils.openLink(hyperlink.getText());
+	}
+}
